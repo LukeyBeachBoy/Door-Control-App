d3e3c457dbac3485b6a74ce1c2fb088aa2e7c2b5
LukeyBeachBoy <lb580@kent.ac.uk>
2019-01-17 20:13:14 +0000

    Working, next update will change the websocket address to the permanent server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { animationFrame } from 'rxjs/internal/scheduler/animationFrame';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'DoorControlApp';
+  socket: WebSocket;
+  title = 'Door Control App';
+  doorStatus = 'Locked';
+  constructor() {
+    this.socket = new WebSocket('ws://desktop-93b8mnp:3000/');
+    this.socket.onmessage = (event) => {
+      console.log(event.data);
+    };
+  }
+  unlock() {
+    this.doorStatus = 'Locked';
+    this.socket.send('unlock');
+  }
+  lock() {
+    this.doorStatus = 'Unlocked';
+    this.socket.send('lock');
+  }
 }
